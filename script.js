3d3df4f2ea23ec93ff313e1e6f21b431f053ebc4
marisystems <215432766+marisystems@users.noreply.github.com>
2026-06-10 10:42:02 -0300

    Add icon and further work on the JS logic

--- script.js
+++ script.js
@@ -20,28 +20,59 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-// Add function to iterate over books in array and display
-// them on the page
-function displayBooks() {
+function getDatafromForm() {
+    //Get node list
+    let formNodes = document.querySelectorAll("#add-book-form input");
+    console.log(formNodes);
+    // Reduce it into an object
 }
 
-addBookToLibrary("Amogus", "sus", 210, true)
-addBookToLibrary("Amogus", "sus", 210, true)
-addBookToLibrary("Amogus", "sus", 210, true)
+// Add function to iterate over books in array and display
+// them on the page
+function displayBooks(library) {
 
-console.log(myLibrary[0].info())
-console.log(myLibrary[0].id);
+    library.forEach( (book)=> {
+        console.log(book)
 
+        // Build the book card html elements dynamically
+        let cardWrapper = document.createElement("div")
+        cardWrapper.classList.add ("book-card-wrapper");
+
+        let card = document.createElement("div")
+        card.classList.add ("book-card")
+
+        let title = document.createElement("p")
+        title.textContent = book.title;
+
+        cardWrapper.appendChild(card);
+        cardWrapper.appendChild(title)
+        bookGrid.appendChild(cardWrapper)
+    })
+    
+}
+
+addBookToLibrary("Wuthering Heights", "sus", 210, true)
+addBookToLibrary("Fahrenheit 451", "sus", 210, true)
+addBookToLibrary("Some cool book", "sus", 210, true)
+
+let bookGrid = document.querySelector(".bottom")
+
+
+displayBooks(myLibrary)
+getDatafromForm()
+
+// Modal
 let addBook = document.querySelector(".add-book");
 let addModal = document.querySelector(".add-modal");
 let closeModal = document.querySelector(".close-modal");
 
 
 // Open the add book modal
-addBook.addEventListener("click", () => {
+addBook.addEventListener("click", () => { 
     addModal.showModal();
 });
 // Close the add book modal
-closeModal.addEventListener("click", () => {
-    addModal.close()
+closeModal.addEventListener("click", (e) => {
+    addModal.close();
 })
+
