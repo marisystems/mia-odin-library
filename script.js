1e0d190e2e0f58b1651686bcc9d0226a22682e7e
marisystems <215432766+marisystems@users.noreply.github.com>
2026-06-10 15:41:22 -0300

    Finish the function to get data from the form
    Made it really dumb and simple because I was really struggling, its
    easier to handle the read radio field because the radio button
    information we are interested is whether it is checked or not

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, cover) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.id = crypto.randomUUID();
+    this.cover = cover;
     if (read) {
         this.read = "read";
     } else {
@@ -15,16 +16,21 @@ function Book(title, author, pages, read) {
     }
 };
 
-function addBookToLibrary(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, read, cover) {
+    let book = new Book(title, author, pages, read, cover);
     myLibrary.push(book);
 }
 
 function getDatafromForm() {
-    //Get node list
-    let formNodes = document.querySelectorAll("#add-book-form input");
-    console.log(formNodes);
-    // Reduce it into an object
+    // Kind of dumb way to do it, but I was getting tripped up
+    // with the nodelist from querySelectorAll
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let cover = document.querySelector("#cover-img").value;
+    let read = document.querySelector("#radio-yes").checked;
+
+    addBookToLibrary(title, author, pages, read, cover);
 }
 
 // Add function to iterate over books in array and display
@@ -32,7 +38,6 @@ function getDatafromForm() {
 function displayBooks(library) {
 
     library.forEach( (book)=> {
-        console.log(book)
 
         // Build the book card html elements dynamically
         let cardWrapper = document.createElement("div")
@@ -57,9 +62,7 @@ addBookToLibrary("Some cool book", "sus", 210, true)
 
 let bookGrid = document.querySelector(".bottom")
 
-
 displayBooks(myLibrary)
-getDatafromForm()
 
 // Modal
 let addBook = document.querySelector(".add-book");
@@ -74,5 +77,7 @@ addBook.addEventListener("click", () => {
 // Close the add book modal
 closeModal.addEventListener("click", (e) => {
     addModal.close();
+    getDatafromForm();
+    console.log(myLibrary)
 })
 
